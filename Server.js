cc86164533ae57d256a992aeab224a5beae9280c
samridhamla06 <samridhamla06@Samridhs-MacBook-Pro.local>
2016-12-06 01:43:39 +0530

    changes

--- Server.js
+++ Server.js
@@ -1,6 +1,6 @@
 var app = require(__dirname + '/app.js');
 var server = require('http').createServer(app);
 
-server.listen(process.env.PORT||9000,function(err){
+server.listen(9000,function(err){
     console.log('Server is running at 8000 port')
 });
