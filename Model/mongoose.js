ecb2eaf1fe102b27faab1a6c2e718e29f4e90fa3
samridhamla06 <samridhamla06@Samridhs-MacBook-Pro.local>
2016-12-05 23:27:12 +0530

    mLAB added

--- Model/mongoose.js
+++ Model/mongoose.js
@@ -1,3 +1,3 @@
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;//to have save() run :)
-module.exports = mongoose.connect('mongodb://localhost/DWARE');//mongoose object
+module.exports = mongoose.connect('mongodb://heroku_dmqgr6px:1ctt8sd2721k9gtbqgmojo4ogl@ds119608.mlab.com:19608/heroku_dmqgr6px');//mongoose object
